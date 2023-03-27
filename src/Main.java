77d1b2b79b790550929a618c447d6ea0504c68ce
Danilo Couto <couto.danilo95@gmail.com>
2023-03-27 18:40:48 -0300

    add classes Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+
+        curso.setTitulo("curso de Java");
+        curso.setDescricao("Bootcamp Curso DIO");
+        curso.setCargaHoraria(1500);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("Bootcamp mentoria DIO");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
     }
 }
